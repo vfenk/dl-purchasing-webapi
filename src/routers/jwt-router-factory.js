59eab542976d3b1b378c7fcb1d039dae0f8a473d
Tris Setiawan <tris.setiawan@yahoo.com>
2017-03-09 03:48:59 +0000

    update jwt-router-factory

--- src/routers/jwt-router-factory.js
+++ src/routers/jwt-router-factory.js
@@ -11,6 +11,8 @@ function getJWTRouter(ManagerType, opts) {
 
     var apiVersion = options.version || "1.0.0";
     var defaultOrder = options.defaultOrder || {};
+    var defaultFilter = options.defaultFilter || {};
+    var defaultSelect = options.defaultSelect || [];
 
     var getManager = (user) => {
         return db.get()
@@ -24,8 +26,11 @@ function getJWTRouter(ManagerType, opts) {
     router.get("/", passport, function(request, response, next) {
         var user = request.user;
         var query = request.query;
-        query.order = Object.assign({}, query.order, defaultOrder, query.order);
 
+        query.filter = Object.assign({}, query.filter, typeof defaultFilter === "function" ? defaultFilter(request, response, next) : defaultFilter, query.filter);
+        query.order = Object.assign({}, query.order, typeof defaultOrder === "function" ? defaultOrder(request, response, next) : defaultOrder, query.order);
+        query.select = query.select ? query.select : typeof defaultSelect === "function" ? defaultSelect(request, response, next) : defaultSelect;
+        
         getManager(user)
             .then((manager) => {
                 return manager.read(query);
